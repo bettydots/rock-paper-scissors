11e9e3884fdd9d77f0e52a2e7ad16bb66f33fea6
bettydots <93158166+bettydots@users.noreply.github.com>
2025-08-04 15:07:16 -0500

    Finished game with 5 round loop
    Transferred playRound() to playGame(). Created initGame() function
    that reduces roundCount, checks to see if there's rounds left,
    reinitializes the choices, runs the game and restarts game for
    next round. Added helper calcWinner() function for when there
    aren't rounds left, which calculates the winner based on each
    players' scores.

--- script.js
+++ script.js
@@ -1,5 +1,38 @@
-const humanChoice = getHumanChoice().toLowerCase()
-const computerChoice = getComputerChoice().toLowerCase()
+let playerScore = 0
+let computerScore = 0
+let roundCount = 5
+initGame()
+
+function initGame() {
+	roundCount--
+	if (roundCount >= 0) {
+		const humanChoice = getHumanChoice().toLowerCase()
+		const computerChoice = getComputerChoice().toLowerCase()
+		console.log(playGame(humanChoice, computerChoice))
+		initGame()
+	} else {
+		calcWinner()
+	}
+}
+
+function playGame(humanChoice, computerChoice) {
+	// let roundCount = 5
+	// for (let i = roundCount; i > 0; i--) {
+	if (humanChoice == computerChoice) {
+		calcScore("")
+		return `We both picked ${computerChoice}. It's a draw`
+	} else if (
+		(humanChoice == "rock" && computerChoice == "paper")
+		|| (humanChoice == "paper" && computerChoice == "scissors")
+		|| (humanChoice == "scissors" && computerChoice == "rock")
+	) {
+		calcScore("computer")
+		return `My ${computerChoice} beats your ${humanChoice}. You lose.`
+	} else {
+		calcScore("player")
+		return `Your ${humanChoice} beats my ${computerChoice}. You win.`
+	}
+}
 
 function getComputerChoice() {
 	let rng = Math.floor((Math.random() * 3) + 1)
@@ -19,26 +52,7 @@ function getHumanChoice() {
 	return window.prompt("Rock, Paper or Scissors?")
 }
 
-function playRound(humanChoice, computerChoice) {
-	if (humanChoice == computerChoice) {
-		calcScore("")
-		return `We both picked ${computerChoice}. It's a draw`
-	} else if (
-		(humanChoice == "rock" && computerChoice == "paper")
-		|| (humanChoice == "paper" && computerChoice == "scissors")
-		|| (humanChoice == "scissors" && computerChoice == "rock")
-	) {
-		calcScore("computer")
-		return `My ${computerChoice} beats your ${humanChoice}. You lose.`
-	} else {
-		calcScore("player")
-		return `Your ${humanChoice} beats my ${computerChoice}. You win.`
-	}
-}
-
 function calcScore(winner) {
-	let playerScore = 0
-	let computerScore = 0
 	if (winner == "computer") {
 		computerScore++
 	} else if (winner == "player") {
@@ -47,4 +61,12 @@ function calcScore(winner) {
 	console.log(`Player: ${playerScore} points. Computer: ${computerScore} points.`)
 }
 
-console.log(playRound(humanChoice, computerChoice))
+function calcWinner() {
+	if (computerScore < playerScore) {
+		console.log("You win!")
+	} else if (playerScore < computerScore) {
+		console.log("You lose!")
+	} else {
+		console.log("It's a draw!")
+	}
+}
